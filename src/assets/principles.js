9964fac871922504d9223b5b35d14b9f1791a471
Mac <mac@akpan.local>
2021-06-04 03:57:42 -0400

    added more data

--- src/assets/principles.js
+++ src/assets/principles.js
@@ -189,9 +189,135 @@ export const indicators = [
     data: "Legal, regulatory, and policy interventions create an enabling environment for REDD+ activities to contribute to the objectives of the National Forest policy"
   },
   {
+    key: "A.1.2",
+    parent: "A.1",
+    data: "Number of REDD+ Leases issued by TLTB"
+  },
+  {
+    key: "A.1.3",
+    parent: "A.1",
+    data: "Number of REDD+ Licenses issued by Ministry of Forestry"
+  },
+  {
+    key: "A.1.4",
+    parent: "A.1",
+    data: "Government Budget for all emission reduction programs and activities across all Govt Dept. including Number of Forestry officers involved in REDD+ activities"
+  },
+  {
+    key: "A.2.1",
+    parent: "A.2",
+    data: "List of legal, regulatory, and policy interventions to create an enabling environment for REDD+ activities to contribute to the objectives National Climate Change, Low Emission Development Strategy and targets"
+  },
+ 
+  {
+    key: "A.2.2",
+    parent: "A.2",
+    data: "Tons of emission reductions achieved by ER Program"
+  },
+  {
+    key: "A.3.1",
+    parent: "A.3",
+    data: "List of legal, regulatory, and policy interventions to create an enabling environment for REDD+ activities to contribute to the objectives National Climate Change, Low Emission Development Strategy and targets"
+  },
+  {
+    key: "A.4.1",
+    parent: "A.4",
+    data: "List of legal, regulatory, and policy interventions to create an enabling environment for REDD+ activities to contribute to the objectives National Climate Change, Low Emission Development Strategy and targets"
+  },
+  {
+    key: "A.4.2",
+    parent: "A.4",
+    data: "List of legal, regulatory, and policy interventions to create an enabling environment for REDD+ activities to contribute to the objectives National Climate Change, Low Emission Development Strategy and targets"
+  },
+  {
+    key: "A.5.1",
+    parent: "A.5",
+    data: "National REDD+ Steering Committee and REDD+ Divisional Working Committees are established and functional"
+  },
+  {
+    key: "A.5.2",
+    parent: "A.5",
+    data: "REDD+ Unit and Divisional REDD+ Team are established and functional"
+  },
+  {
     key: "B.1.1",
     parent: "B.1",
     data: "Legal framework is in place to support procedures for accessing information about REDD+ policy, strategies and activities and multiple distributions channels of information"
+  },
+  {
+    key: "B.1.2",
+    parent: "B.1",
+    data: "Information on REDD+ policy, strategies and activities design, implementation and results is publicly accessible "
+  },
+  {
+    key: "B.2.1",
+    parent: "B.2",
+    data: "Results-based payment are attributed in a transparent and accountable manner according to the benefit sharing plan"
+  },
+  {
+    key: "B.2.2",
+    parent: "B.2",
+    data: "Sustainable financing mechanism secured for REDD+ activities"
+  },
+  {
+    key: "B.2.3",
+    parent: "B.2",
+    data: "Ministry of Forestry REDD+ Unit submit annual audit reports"
+  },
+  {
+    key: "B.3.1",
+    parent: "B.3",
+    data: "The process outlined in the REDD+ FGRM is adopted and implemented to resolve grievances"
+  },
+  {
+    key: "B.3.2",
+    parent: "B.3",
+    data: "Number of grievances that are registered and resolved in a timely manner, under each thematic areas as defined in the FGRM"
+  },
+  {
+    key: "B.3.3",
+    parent: "B.3",
+    data: "Number of disputes resolved through informal and traditional networks"
+  },
+  {
+    key: "B.4.1",
+    parent: "B.4",
+    data: "Target 33% women in decision making positions at District level by 2025"
+  },
+  {
+    key: "B.4.2",
+    parent: "B.4",
+    data: "REDD+ Gender Guideline is implemented by ERP"
+  },
+  {
+    key: "B.5.1",
+    parent: "B.5",
+    data: "National REDD+ Steering Committee meeting outcome are shared publically and adhere to the internal regulations"
+  },
+  {
+    key: "B.5.2",
+    parent: "B.5",
+    data: "Divisional Working Committees meeting outcome are shared publically and adhere to the internal regulations"
+  },
+  {
+    key: "B.6.1",
+    parent: "B.6",
+    data: "Divisional Working Committees meeting outcome are shared publically and adhere to the internal regulations"
+  },
+  {
+    key: "B.6.2",
+    parent: "B.6",
+    data: "Number of  Forestry  related offences monitored and reported"
+  },
+  {
+    key: "B.6.3",
+    parent: "B.6",
+    data: "REDD+ activity area have well documented patrolling  and reporting framework"
+  },
+  {
+    key: "C.1.2",
+    parent: "C.1",
+    data: "Proportion of customary and statutory land and resources mapped where REDD+ activities are implemented"
   },
   {
     key: "C.1.1",
@@ -199,9 +325,84 @@ export const indicators = [
     data: "Procedures are in place for the mapping  and registering of customary and statutory land and resource rights where REDD+ activities are implemented"
   },
   {
+    key: "C.2.1",
+    parent: "C.2",
+    data: "Customary/statutory land and resource rights are recognised and implemented through  REDD+ leases and licensing procedures"
+  },
+  {
+    key: "C.2.2",
+    parent: "C.2",
+    data: "Number of REDD+ Licenses issued to all beneficiaries (iTaukei, statutory and private land) by the Ministry of Forestry"
+  },
+  {
+    key: "C.2.3",
+    parent: "C.2",
+    data: "Number of REDD+ Licenses issued to all beneficiaries of iTaukei land by the Ministry of Forestry"
+  },
+  {
+    key: "C.2.4",
+    parent: "C.2",
+    data: "Number of Deed of Trust registered by TLTB and Land Bank to support communal involvement in REDD+ Leases"
+  },
+  {
+    key: "C.3.1",
+    parent: "C.3",
+    data: "The REDD+ Benefit Sharing Plan is adopted and implemented "
+  },
+  {
+    key: "C.3.2",
+    parent: "C.3",
+    data: "Number of eligible beneficiaries of the REDD+ program that receive benefits according to the Benefit Sharing Plan"
+  },
+  {
+    key: "C.4.1",
+    parent: "C.4",
+    data: "Legal framework is in place to ensure that no involuntary relocation took place without the free, prior, and informed consent (FPIC) of any indigenous peoples and local communities (or equivalent) concerned."
+  },
+  {
+    key: "C.4.2",
+    parent: "C.4",
+    data: "Number of relocation cases due to the implementation of the REDD+ activities and resettlement action plan approved and implemented in alignment to Legal Framework"
+  },
+  {
+    key: "C.5.1",
+    parent: "C.5",
+    data: "Consent is obtained after provision of full information for REDD+ activities on land traditionally used by iTaukei"
+  },
+  {
+    key: "C.5.2",
+    parent: "C.5",
+    data: "REDD+ activities that contribute to conservation of cultural and natural heritage sites are documented, mapped and respected"
+  },
+  {
     key: "D.1.1",
     parent: "D.1",
     data: "Stakeholder analysis is completed  for REDD+ activities"
+  },
+  {
+    key: "D.2.1",
+    parent: "D.2",
+    data: "Legal framework exists to enable and ensure full, effective and timely participation of REDD+ communities and iTaukei in the design and implementation of REDD+ activities"
+  },
+  {
+    key: "D.2.2",
+    parent: "D.2",
+    data: "Number of consultation, awareness and education of relevant stakeholders that have participated fully and effectively in the design and implementation of REDD+ activities."
+  },
+  {
+    key: "D.2.3",
+    parent: "D.2",
+    data: "Number of consultation, awareness and education of relevant stakeholders that have participated fully and effectively in the design and implementation of REDD+ activities."
+  },
+  {
+    key: "D.3.1",
+    parent: "D.3",
+    data: "Stakeholders representatives participate in meetings, in accordance to their respective rights and decision-making structures and procedures"
+  },
+  {
+    key: "D.4.1",
+    parent: "D.4",
+    data: "Proportion of  REDD+ documents are available in iTaukei, English and Fiji-Hindi"
   },
   {
     key: "E.1.1",
@@ -209,14 +410,74 @@ export const indicators = [
     data: "Legal framework exists to support establishment of High Conservation Value Forest to include Key Biodiversity Area"
   },
   {
+    key: "E.1.2",
+    parent: "E.1",
+    data: "Biodiversity and ecosystem services potentially affected by REDD+ activities are identified, mapped and appropriately managed"
+  },
+  {
+    key: "E.2.1",
+    parent: "E.2",
+    data: "Monitoring of impacts of REDD+ programs on natural forest demonstrates there is no conversion "
+  },
+  {
+    key: "E.3.1",
+    parent: "E.3",
+    data: "Projects in REDD+ sites have completed a management plan addressing any potential adverse environmental impact"
+  },
+  {
+    key: "E.3.2",
+    parent: "E.3",
+    data: "Area of High Conservation Value Forests identified and the number maintained by the REDD+ program is known"
+  },
+  {
+    key: "E.4.1",
+    parent: "E.4",
+    data: "Percentage of households with increased income through REDD+ implementation"
+  },
+  {
+    key: "E.4.2",
+    parent: "E.4",
+    data: "Percentage of result based incentives that flows to the LOU+members of immediate community from the REDD+ activities"
+  },
+  {
+    key: "E.4.3",
+    parent: "E.4",
+    data: "Proportion of households that have access to adequate food of acceptable quality and nutritional value in the REDD+ districts"
+  },
+  {
     key: "F.1.1",
     parent: "F.1",
     data: "Direct and indirect drivers of deforestation and forest degradation are clearly identified"
   },
   {
+    key: "F.1.2",
+    parent: "F.1",
+    data: "Measures to address the drivers have been identified and integrated in the design, prioritization, implementation, and periodic assessments of REDD+ activities"
+  },
+  {
+    key: "F.2.1",
+    parent: "F.2",
+    data: "A National Forest Inventory and reference level are completed and forest cover changes are reported annually"
+  },
+  {
+    key: "F.2.2",
+    parent: "F.2",
+    data: "Number of staff/forestry officers participating in capacity building programs for MRV"
+  },
+  {
     key: "G.1.1",
     parent: "G.1",
     data: "Number of District in Integrated Land Use Plans are developed through a participatory process to guide the zonation of potential REDD+ activites"
+  },
+  {
+    key: "G.2.1",
+    parent: "G.2",
+    data: "Number of District in Integrated Land Use Plans are developed through a participatory process to guide the zonation of potential REDD+ activites"
+  },
+  {
+    key: "G.2.2",
+    parent: "G.2",
+    data: "Non-carbon benefits to incentivise beneficiaries to implement additional REDD+ activities"
   },
 ]
 
@@ -301,6 +562,73 @@ export const indicatorDetails = [
     linkage: "REDD+ MRV system"
   },
   {
+    parent: "A.3.1",
+    LOA: "National",
+    definition: "Assesses the contribution of REDD+ activities to the overall budget based on NDP and GGF",
+    verifier: "Share of national budget dedicated to REDD+",
+    source: "National Budget based on NDP and GGF",
+    methodology: "Ministry of Economy shares annual budget with Ministry of Forestry",
+    collects: "REDD+ Unit team",
+    validates: "REDD+ SC",
+    dataInSIS: `% of national budget towards ERP and other related REDD+ activities`,
+    condition: "high/medium/low",
+    linkage: "n/a"
+  },
+  {
+    parent: "A.4.1",
+    LOA: "National",
+    definition: "Assess REDD+ activities overlap with biodiversity conservation areas",
+    verifier: "Share of national budget dedicated to REDD+",
+    source: "Maps of Key Biodiveristy Areas aligned to Network of Protected Area under NBSAP, maps of REDD+ activities",
+    methodology: "REDD+ Unit compares Network of PA under NBSAP and REDD+ activities maps",
+    collects: "Min of Environment, Min of Forestry",
+    validates: "REDD+ Unit",
+    dataInSIS: `Map of overlapping thematic area maps from NBSAP and areas under ERP`,
+    condition: "high/medium/low",
+    linkage: "NBSAP"
+  },
+  {
+    parent: "A.4.2",
+    LOA: "National",
+    definition: "Assesses the number of protected areas created as a results of REDD+ activities",
+    verifier: "Report of the REDD+ Unit on the implementation of ERP activities",
+    source: "ER Project document",
+    methodology: "Desktop, ER proposed document and Annual Report",
+    collects: "Divisional officer, Project Lead and partners",
+    validates: "REDD+ Unit, Conservator of Forests  ",
+    dataInSIS: `Location and area covered`,
+    condition: "5ha<conservation area<10000ha",
+    linkage: "Ministry of Forestry Website, Ministry of Environment Website"
+  },
+  {
+    parent: "A.5.1",
+    LOA: "National",
+    definition: "Assess the exsitence and operationalization of REDD+ governance structures",
+    verifier: `ToRs of REDD+ Steering Committee and REDD+ Provincial Working Committess
+    Number of yearly meetings`,
+    source: "Ministry of Forestry, REDD+ Unit",
+    methodology: "REDD+ Unit and REDD secretariat collects and provide ToRs, Meeting report and minutes",
+    collects: "REDD+ Unit/DFRAC",
+    validates: "REDD+ SC",
+    dataInSIS: `Number of National REDD+ Steering Committee and REDD+ Provincial Working Committees meetings`,
+    condition: "Yes or No",
+    linkage: "Ministry of Forestry website"
+  },
+  {
+    parent: "A.5.2",
+    LOA: "National",
+    definition: "Assess the exsitence and operationalization of REDD+management structures",
+    verifier: `ToRs of REDD+Unit and Divisional working groups
+    Annual activity reports`,
+    source: "Ministry of Forestry, REDD+ Unit",
+    methodology: "Annual activity reports of RED+ Unit and Divisional REDD+ Teams",
+    collects: "REDD+ Unit/DFRAC",
+    validates: "REDD+ SC",
+    dataInSIS: `Annual activity reports`,
+    condition: "Existance of Unit: Yes or No",
+    linkage: "Ministry of Forestry website"
+  },
+  {
     parent: "B.1.1",
     LOA: "National",
     definition: "Legal framework support access to information",
@@ -325,5 +653,32 @@ export const indicatorDetails = [
     dataInSIS: `All, some or none of the following documents are published ("Required High - all  Medium - some  Low - None) For each project: proposal, annual report, REDD+ lease For national level: strategy, ERPD, SESA, ESMF`,
     condition: "High, if all information are publicly accessible;  Medium if some information are publicly accessible and; Low if no information is publically accessible",
     linkage: "MoF website, MoEco website, MoA website, iTltb websites"
+  },
+  {
+    parent: "B.2.1",
+    LOA: "National and Activity",
+    definition: "Payment of REDD+ result based effort, is aimed to incentivise good practices for the reduction of emission, so it is  important that payment falls in the right hands and done in a fair and transparent manner according to the Benefit Sharing Plan",
+    verifier: `ER Payment ledgers and Payment Report 
+    Annual BSP audited report`,
+    source: "Annual Report, BSP audited report",
+    methodology: "MoF Annual Report, ER Report",
+    collects: "Admin-Finance team, REDD+Unit",
+    validates: "REDD+ SC, DFRAC",
+    dataInSIS: `Annual BSP audited report Yes/No`,
+    condition: "Yes or No",
+    linkage: "MoF website"
+  },
+  {
+    parent: "B.2.2",
+    LOA: "National",
+    definition: "Access the long term contribution of REDD+ activities to ensure sustainability of protected areas",
+    verifier: `MoForestry License Register`,
+    source: "TLTB Lease Registry, Landowners Unit Affairs",
+    methodology: "TLTB data base and registry information circulated",
+    collects: "REDD+ unit, DFRAC",
+    validates: "REDD+ SC",
+    dataInSIS: `Lease rental and associated income to landowners`,
+    condition: "Yes/No on the issue of REDD+ Conservation Leases",
+    linkage: "TLTB website"
   },
 ]
