d6fc85e3c867ee2beb3da3ce48271d267952a7d2
Mac <mac@akpan.local>
2021-06-07 14:08:43 +0100

    added scrollable columns

--- src/components/PageWrapper/style.js
+++ src/components/PageWrapper/style.js
@@ -1,16 +1,13 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
-    padding-left: 50px;
-    padding-right: 50px;
+    padding-left: 30px;
+    padding-right: 30px;
     background: #8E966F;
     height: 100vh;
-    overflow-y: scroll;
+    position: fixed;
 
     .contents {
-        border-left: 1px solid #edf5ea;
-        border-right: 1px solid #edf5ea;
-        border-bottom: 1px solid #edf5ea;
-        min-height: 93vh;
+       height: 100vh;
     }
 `
